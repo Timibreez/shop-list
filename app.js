90bb5367dc074b04fee5f17908e675a8f9bd5ad5
Timibreez <ffftimi@gmail.com>
2022-03-13 00:00:39 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+// Change the URL of the route to the root URL
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(3000);
