458865ffa43daf80d77e70a8954707af6bd1e654
Timibreez <ffftimi@gmail.com>
2022-03-27 13:30:59 +0100

    updating list from mysql

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -18,7 +19,17 @@ app.get('/', (req, res) => {
 
 app.get('/create', (req, res) => {
   res.render('create.ejs')
-})
+});
+
+app.post('/new', (req, res) =>{
+  connection.query(
+    'INSERT INTO items (name) VALUES(?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
 
 app.get('/index', (req, res) => {
   connection.query('SELECT * FROM shop_list.items', (error, results) => {
